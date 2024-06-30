62e285fecc8347251f4fbda97f825082a085e4bd
Borya Frolov <bfrolov@renins.com>
2024-06-30 19:05:06 +0300

    the solution

---
+++  ParkingBill/ ParkingBill/Parking.cs
@@ -0,0 +1,49 @@
+﻿namespace ParkingBill;
+
+public static class Parking
+{
+    private const int EntranceFee = 2;
+
+    private const int FirstHourCost = 3;
+
+    private const int HourCost = 4;
+
+    public static int CalculateCost(string timeEntered, string timeLeft)
+    {
+        var additionalHours = GetAdditionalHours(timeEntered, timeLeft);
+
+        var cost = EntranceFee + FirstHourCost + HourCost * additionalHours;
+
+        return cost;
+    }
+
+    private static int GetAdditionalHours(string timeEntered, string timeLeft)
+    {
+        var hourEntered = int.Parse(timeEntered.Split(":").First());
+        var minuteEntered = int.Parse(timeEntered.Split(":").Last());
+        
+        var hourLeft = int.Parse(timeLeft.Split(":").First());
+        var minuteLeft = int.Parse(timeLeft.Split(":").Last());
+
+        var minutesSpent = (hourLeft - hourEntered) * 60 - minuteEntered + minuteLeft;
+        
+        // minus first hour
+        var additionalMinutesSpent = minutesSpent - 60;
+        
+        // partial hour
+        if (additionalMinutesSpent <= 0)
+        {
+            return 0;
+        }
+
+        var additionalHoursSpent = additionalMinutesSpent / 60;
+        
+        //check last partial hour
+        if (additionalMinutesSpent - additionalHoursSpent * 60 > 0)
+        {
+            additionalHoursSpent += 1;
+        }
+
+        return additionalHoursSpent;
+    }
+}
---
+++  ParkingBill/ParkingBill.Tests/ParkingTests.cs
@@ -0,0 +1,80 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace ParkingBill.Tests;
+
+[TestFixture]
+public class ParkingTests
+{
+    [Test]
+    public void CalculateCost_Test1()
+    {
+        var cost = Parking.CalculateCost("10:00", "13:21");
+
+        cost.Should().Be(17);
+    }
+
+    [Test]
+    public void CalculateCost_Test2()
+    {
+        var cost = Parking.CalculateCost("09:42", "11:42");
+
+        cost.Should().Be(9);
+    }
+
+    [Test]
+    public void CalculateCost_Test3()
+    {
+        var cost = Parking.CalculateCost("09:15", "09:42");
+
+        cost.Should().Be(5);
+    }
+
+    [Test]
+    public void CalculateCost_Test4()
+    {
+        var cost = Parking.CalculateCost("09:00", "10:00");
+
+        cost.Should().Be(5);
+    }
+
+    [Test]
+    public void CalculateCost_Test5()
+    {
+        var cost = Parking.CalculateCost("09:00", "09:59");
+
+        cost.Should().Be(5);
+    }
+
+    [Test]
+    public void CalculateCost_Test6()
+    {
+        var cost = Parking.CalculateCost("09:59", "10:00");
+
+        cost.Should().Be(5);
+    }
+
+    [Test]
+    public void CalculateCost_Test7()
+    {
+        var cost = Parking.CalculateCost("09:59", "11:00");
+
+        cost.Should().Be(9);
+    }
+
+    [Test]
+    public void CalculateCost_Test8()
+    {
+        var cost = Parking.CalculateCost("09:59", "11:59");
+
+        cost.Should().Be(9);
+    }
+
+    [Test]
+    public void CalculateCost_Test9()
+    {
+        var cost = Parking.CalculateCost("10:00", "11:01");
+
+        cost.Should().Be(9);
+    }
+}
